d20186b88b6076dfac322fd2e0e406d98095be72
Bob Gardner <bob.hn.gardner@gmail.com>
2018-11-29 22:24:10 -0800

    Stub out VirtualRepainterViewModel

--- KinectDrawing/VirtualRepainter/VirtualRepainterViewModel.cs
+++ KinectDrawing/VirtualRepainter/VirtualRepainterViewModel.cs
@@ -1,16 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using KinectDrawing;
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
 
 namespace VirtualRepainter
 {
-    public class VirtualRepainterViewModel : BindableBase
+    public class VirtualRepainterViewModel : INotifyPropertyChanged
     {
         public VirtualRepainterViewModel()
         {
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string HeaderText { get; private set; }
+        public string SubHeaderText { get; private set; }
+
+        public ImageSource CameraImageSource { get; }
+
+        public Visibility PersonOutlineVisibility { get; private set; } = Visibility.Visible;
+        public string CountdownValue { get; private set; }
+
+        public Visibility UserPointerVisibility { get; private set; } = Visibility.Collapsed;
+        public int UserPointerPositionX { get; private set; }
+        public int UserPointerPositionY { get; private set; }
     }
 }
