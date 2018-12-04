b1667a61b6115af7c0e3c01d93398d75045aac1d
Bob Gardner <bob.hn.gardner@gmail.com>
2018-12-04 01:43:26 -0800

    WIP: Add VirtualPainting UnitTests

---
+++ KinectDrawing/VirtualPainting.UnitTests/PersonDetectionStateTests.cs
@@ -0,0 +1,80 @@
+﻿using System.Windows;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VirtualPainting.UnitTests
+{
+    [TestClass]
+    public class PersonDetectionStateTests
+    {
+        [TestMethod]
+        public void PersonDetectionState_Equivalence()
+        {
+            var bodyPresenceArea = new Rect();
+            var newUserButtonArea = new Rect();
+            var state = new PersonDetectionState(
+                bodyIndex: 0,
+                isPrimary: true,
+                body: null,
+                bodyPresenceArea: bodyPresenceArea,
+                newUserButtonArea: newUserButtonArea);
+
+            // Reflexive
+            Assert.IsTrue(state.Equals(state));
+#pragma warning disable CS1718 // Comparison made to same variable
+            Assert.IsTrue(state == state);
+#pragma warning restore CS1718 // Comparison made to same variable
+            Assert.AreEqual(state, state);
+            Assert.AreSame(state, state);
+
+            var stateCopy1 = new PersonDetectionState(
+                bodyIndex: 0,
+                isPrimary: true,
+                body: null,
+                bodyPresenceArea: bodyPresenceArea,
+                newUserButtonArea: newUserButtonArea);
+
+            // Symmetric
+            Assert.AreEqual(stateCopy1.Equals(state), state.Equals(stateCopy1));
+            Assert.AreEqual(stateCopy1 == state, state == stateCopy1);
+
+            // Transitive
+            var stateCopy2 = new PersonDetectionState(
+                bodyIndex: 0,
+                isPrimary: true,
+                body: null,
+                bodyPresenceArea: bodyPresenceArea,
+                newUserButtonArea: newUserButtonArea);
+
+            Assert.AreEqual(state.Equals(stateCopy2), state.Equals(stateCopy1) && stateCopy1.Equals(stateCopy2));
+            Assert.AreEqual(state == stateCopy2, state == stateCopy1 && stateCopy1 == stateCopy2);
+        }
+
+        [TestMethod]
+        public void PersonDetectionState_Correctness()
+        {
+            var state = new PersonDetectionState(
+                bodyIndex: 0,
+                isPrimary: true,
+                body: null,
+                bodyPresenceArea: new Rect(),
+                newUserButtonArea: new Rect());
+
+            Assert.IsFalse(state.Equals(null));
+            Assert.IsFalse(state == null);
+            Assert.AreNotEqual(null, state);
+            Assert.AreNotSame(null, state);
+
+            var stateCopy = new PersonDetectionState(
+                bodyIndex: 1,
+                isPrimary: false,
+                body: null,
+                bodyPresenceArea: new Rect(),
+                newUserButtonArea: new Rect());
+
+            Assert.IsFalse(state.Equals(stateCopy));
+            Assert.IsFalse(state == stateCopy);
+            Assert.AreNotEqual(stateCopy, state);
+            Assert.AreNotSame(stateCopy, state);
+        }
+    }
+}
---
+++ KinectDrawing/VirtualPainting.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VirtualPainting.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VirtualPainting.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b43a4c21-9f84-40bd-bc88-dcce4b75abce")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
